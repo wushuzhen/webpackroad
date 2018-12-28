d12ded37f8be3a5133570fa06167ef90478ec8ca
吴先生 <2429190475@qq.com>
2018-12-28 21:58:12 +0800

    tubiao

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import $ from 'jquery'
 import './src/css/index.css'
 import './src/css/1.less'
 // import './src/css/2.scss'
-// import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 $(function () {
   $('li:odd').css('backgroundColor', 'red')
--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
         ]
       },
       {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
         use: ['url-loader']
       }
     ]
